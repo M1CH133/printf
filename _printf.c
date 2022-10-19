b6ae84e49bef64dfcc1184a28f5476eee8646a75
Njambiii <111266752+Njambiii@users.noreply.github.com>
2022-10-20 01:23:05 +0300

    Task 0

--- _printf.c
+++ _printf.c
@@ -1,28 +1,38 @@
 #include "main.h"
 
 /**
-* _printf - Receives the main string and all the necessary parameters to
-* print a formated string
-* @format: A string containing all the desired characters
-* Return: A total count of the characters printed
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
 */
 
 int _printf(const char *format, ...)
 {
-int printed_characters;
+int characters_printed;
 conver_t f_list[] = {
 {"c", print_char},
 {"s", print_string},
 {"%", print_percent},
+{"d", print_integer},
+{"i", print_integer},
+{"u", unsigned_integer},
+{"b", print_binary},
+{"r", print_reversed},
+{"o", print_octal},
+{"x", print_hex},
+{"X", print_heX},
+{"R", rot13},
+{NULL, NULL}
 };
-
+  
 va_list arg_list;
 if (format == NULL)
 return (-1);
 va_start(arg_list, format);
-
+  
 /*Calling parser function*/
-printed_characters = parser(format, f_list, arg_list);
+characters_printed = parser(format, f_list, arg_list);
 va_end(arg_list);
-return (printed_characters);
+return (characters_printed);
 }
