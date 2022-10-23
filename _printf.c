2394ae50cc7b9e7b1c33cc370c5540d5323922a2
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2022-10-23 21:33:43 +0300

    Fixing bugs

--- _printf.c
+++ _printf.c
@@ -1,8 +1,12 @@
 #include "main.h"
+
 /**
  * _printf - produces output according to a format
- * @format: is a character string composed of zero or more directives
- * Return: the number of characters printed 
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
